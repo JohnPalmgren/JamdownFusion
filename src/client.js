d9965d45b8da86f4287f9f98de0d5f82b1aca66b
John <johnmpalmgren@gmail.com>
2022-03-03 12:06:14 +0000

    update client variables

--- src/client.js
+++ src/client.js
@@ -1,7 +1,6 @@
-// https://www.youtube.com/watch?v=PB5RQk5gAdQ&t=1184s
 import * as contentful from "contentful";
 
 export const client = contentful.createClient({
-  space: "y71dqeozxnut",
-  accessToken: "4Y28dbbuoit_k02MrTu-wmK5ciI8ld1uj_eKeRSolyo"
+  space: process.env.REACT_APP_SPACE_ID,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
